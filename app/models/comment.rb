474d0455f57d22503c88c6c351204ca7a31c4f39
Susumu <sumura80@gmail.com>
2018-10-03 21:16:47 +0900

    add sql in comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,6 +7,9 @@ class Comment < ApplicationRecord
   # こうすることにより、複雑なSQLを何度も書かなくてよくなり、可読性と保守性を向上させることができます。
   # クエリとは、簡単にいうとターミナルやコマンドプロンプトに表示されるSQLのことだと考えてもらって問題ないです。
   # ここでvotesの数で並び替える処理をscopeとして定義している。
-  scope :order_by_voute_count, ->  { eager_load(:votes).group("comments.id").order('count(votes.id) desc') }
+  #scope :order_by_voute_count, ->  { eager_load(:votes).group("comments.id").order('count(votes.id) desc') }
   # eager_loadは外部結合ができるメソッドです。
+
+
+  scope :order_by_voute_count, ->  { left_joins(:votes).group(:id).order('count(votes.id) desc') }
 end
